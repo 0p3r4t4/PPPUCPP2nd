d808dcf1a4e4a8cbfde119541bf70fbc231c3271
Dan González Rodríguez <gonrodda@gmail.com>
2016-10-27 00:56:34 +0200

    Chapter 4, exercise 15 finished

--- ch04/4.exercise.15.cpp
+++ ch04/4.exercise.15.cpp
@@ -6,8 +6,8 @@
 // Comments:
 //  I don't think that the Sieve of Eratosthenes is useful in this problem.
 //  We need to simply iterate over natural numbers testing them being prime or
-//  not, until we found the desired number of primes. So te solution from
-//  exercise 12 feest better.
+//  not, until we found the desired number of primes. So the solution from
+//  exercise 12 fits better.
 
 #include "std_lib_facilities.h"
 
@@ -30,16 +30,22 @@ int main()
     vector<int> primes;
     primes.push_back(2);
     
-    cout << "Find prime numbers from 1 to? ";
-    int max {0};
-    cin >> max;
+    cout << "How many prime numbers do you want to find? ";
+    int limit {0};
+    cin >> limit;
 
-    for (int i=3; i <= max; ++i) {
-        if (is_prime(i, primes))
+    int i {3};
+    int count {1};
+
+    while (count < limit) {
+        if (is_prime(i, primes)) {
             primes.push_back(i);
+            ++count;
+        }
+        ++i;
     }
 
-    cout << "Primes between 1 and " << max << ":\n";
+    cout << "The " << limit << " first prime numbers:\n";
     for (int x : primes)
         cout << x << '\n';
     
