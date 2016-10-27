51536209d9c8d5e644e78c5be0b7d98c816123f1
Dan González Rodríguez <gonrodda@gmail.com>
2016-10-28 01:52:25 +0200

    Chapter 4, exercise 19, problem statement to start and continue tomorrow

---
+++ ch04/4.exercise.19.cpp
@@ -0,0 +1,15 @@
+// 4.exercise.19.cpp
+//
+// Write a program where you first enter a set of name-and-value pairs, such as
+// Joe 17 and Barbara 22. For each pair, add the name to a vector called names
+// and the numbers to a vector called scores (in corresponding positions, so
+// that if names[7]=="Joe" the scores[7]==17). Terminate input with NoName 0.
+// Check that each name is unique and terminate with an error message if a name
+// is entered twice. Write out all the (name, score) pairs (one per line).
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    return 0;
+}
