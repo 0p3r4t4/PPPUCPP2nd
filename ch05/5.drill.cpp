d9e6b221da7fc1dd70cfae93deca05e588cf7714
0p3r4t4 <gonrodda@gmail.com>
2016-11-11 02:19:13 +0100

    Completed drill from chapter 5

--- ch05/5.drill.cpp
+++ ch05/5.drill.cpp
@@ -105,7 +105,7 @@ try {
 
     // Fragment 10 (Logical error)
     // Original -> Prints nothing. We can change the string literals or the
-    // logic behind de condition
+    //             logic behind de condition
     // string s10 = "ape";
     // if (s10 == "fool") cout << "Success!\n";
     // Fixed
@@ -131,6 +131,9 @@ try {
     //             ‘std::basic_string<char>’ to ‘bool’
     //          -> error: no match for ‘operator<’ (operand types are
     //             ‘std::ostream {aka std::basic_ostream<char>}’ and ‘const char[10]’)
+    // string s12 = "ape";
+    // if (s12 + "fool") cout < "12. Success!\n";
+    // Fixed
     string s12 = "ape";
     if (s12 != "fool") cout << "12. Success!\n";
 
@@ -199,13 +202,130 @@ try {
     //             string out of bounds. Correctly means luckily. std::string
     //             do not perform out of bounds checking with [] operator, so
     //             not exception is thrown.
-    // string s18 = "Success!\n";
+    // string s18 = "18. Success!\n";
     // for (int i=0; i<1109; ++i)
     //     cout << s18[i];
     // Fixed
     string s18 = "18. Success!\n";
     for (size_t i=0; i<s18.size(); ++i)
         cout << s18[i];
+
+    // Fragment 19 (Compile-time error)
+    // Original -> error: missing template arguments before ‘v19’
+    //          -> again, one more iteration than expected by making the
+    //             for-statement condition <= instead of <
+    // vector v19(5);
+    // for (int i=0; i<=v19.size(); ++i)
+    //     cout << "19. Success!\n";
+    // Fixed
+    vector<int> v19(5);
+    for (size_t i=0; i<v19.size(); ++i)
+        cout << "19. Success!\n";
+
+    // Fragment 20 (Logical error)
+    // Original -> Infinite while loop, varying value of j but evaluating 
+    //             condition with i.
+    // int i20 = 0;
+    // int j20 = 9;
+    // while (i20 < 10)
+    //    ++j20;
+    // if (j20 < i20)
+    //    cout << "20. Success!\n";
+    // Fixed
+    int i20 = 0;
+    int j20 = 9;
+    while (i20 < 10)
+        ++i20;
+    if (j20 < i20)
+        cout << "20. Success!\n";
+
+    // Fragment 21 (Run-time error and more)
+    // Original -> Floating point exception (Division by 0)
+    //          -> Not the only error, read further
+    // Comments:
+    //  This one makes me feel stupid. In fact, I can't get what this piece of 
+    //  code is intended to do, and it has more than one problem.
+    //  At first I assume that it intends to test if 2 is a solution to a 
+    //  quadratic equation, 2x²-3.5x-6=0 (as infered from
+    //  5/(x-2)=2x+0.5). Using the exercise 18 from chapter 4, the solutions are
+    //  x = 2.81552 and x = -1.06552. So making x double and value 2.81552
+    //  should work, but it doesn't. See
+    //  https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
+    //  to know why. The solution to this is far more complex that I'm able to
+    //  assume at this point of the book.
+    //  So, I search the web and found solutions that modify the value of x and
+    //  the mathematical expressions to the point to be barely recognizable.
+    //  They work but dont's seem very sound to me. 
+    //  What have we done to this point in the drill? Fix kinda silly errors.
+    //  Is not logical to think this way and reduce everything to a pair of
+    //  typographic mistakes?
+    //  What happens if we modify the assignment:
+    //      d = 5/(x-2)
+    //  to
+    //      d = 5.0/x+2
+    //  the exclusion of parenthesis and the subtitution of - by + seemss to me
+    //  like a probable mistake. 
+    //  Of course we must include a float point value for the expression to
+    //  evaluate in float point as a whole instead of as integers and cause
+    //  a narrow result.
+    //
+    // int x21 = 2;
+    // double d21 = 5/(x21-2);
+    // if (d21 == 2*x21+0.5)
+    //     cout << "21. Success!\n";
+    // Fixed
+    int x21 = 2;
+    double d21 = 5.0/x21+2;
+    if (d21 == 2*x21+0.5)
+        cout << "21. Success!\n";
+    
+    // Fragment 22 (Compile-time error)
+    // Original -> error: ‘std::string {aka std::basic_string<char>}’ is not
+    //             a template
+    // string<char> s = "Success!\n";
+    // for (int i=0; i<=10; ++i)
+    //     cout << s[i];
+    // Fixed
+    string s22 = "22. Success!\n";
+    for (size_t i=0; i<s22.size(); ++i)
+        cout << s22[i];
+
+    // Fragment 23 (Compile-time error and more)
+    // Original -> error: ‘j23’ was not declared in this scope
+    //          -> simply declaring j give us a infinite while loop
+    // int i23 = 0;
+    // while (i23 < 10)
+    //     ++j23;
+    // if (j23 < i23)
+    //     cout << "23. Success!\n";
+    // Fixed
+    int i23 = 0;
+    int j23 = 0;
+    while (i23 < 10)
+        ++i23;
+    if (j23 < i23)
+        cout << "23. Success!\n";
+
+    // Fragment 24 (See fragment 21)
+    // Original -> It works at first try, just becuase the if condition
+    //             is an assignment. We can not consider it correct.
+    // int x24 = 4;
+    // double d24 = 5/(x24-2);
+    // if (d24 = 2*x24+0.5)
+    //     cout << "24. Success!\n";
+    // Fixed
+    int x24 = 4;
+    double d24 = 5.0/(x24-2);
+    if (d24 == x24/2.0+0.5)
+        cout << "24. Success!\n";
+
+    // Fragment 25 (Compile-time error)
+    // Original -> error: no match for ‘operator<<’ (operand types are
+    //             ‘std::istream {aka std::basic_istream<char>}’ and ‘const
+    //             char [14]’)
+    // cin << "25. Success!\n";
+    // Fixed
+    cout << "25. Success!\n";
 
     return 0;
 }
