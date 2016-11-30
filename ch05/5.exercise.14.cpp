076d9907ce15eaa7dd944a079246ffc56a6aa70f
0p3r4t4 <gonrodda@gmail.com>
2016-11-30 15:06:07 +0100

    Remove glossary (not maintained), new entries in notes.txt and enunciate exercise 14 from chapter 5.

---
+++ ch05/5.exercise.14.cpp
@@ -0,0 +1,12 @@
+// 5.exercise.14.cpp
+//
+// Read (day-of-the-week, calue) pairs from standard input. For example:
+//
+//  Tuesday 24 Friday 56 Tuesday -3 Thursday 99
+//
+// Collect all the values for each day of the week in a vector<int>. Write out the
+// values of the seven day-of-the-week vectors. Print out the sum of the values
+// in each vector. Ignore illegal days of the week, such as Funday, but accept 
+// common synonyms such as Mon and monday. Write out the number of rejected values.
+
+#include "std_lib_facilities.h"
