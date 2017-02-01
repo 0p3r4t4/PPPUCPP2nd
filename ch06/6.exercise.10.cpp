0899ae1b785bd7f9a7d8597c037f6e0ce2e5b1cf
0p3r4t4 <gonrodda@gmail.com>
2017-02-01 18:54:18 +0100

    Proofs of concept.

--- ch06/6.exercise.10.cpp
+++ ch06/6.exercise.10.cpp
@@ -5,7 +5,9 @@
 // three different numbers for the combination. There are P(60,3) permutations
 // for the combination, where P is defined by the formula
 // 
-//                a!  P(a,b) = ------, (a-b)!
+//                a! 
+//     P(a,b) = ------,
+//              (a-b)!
 // 
 // where ! is used as a suffix factorial operator. For example, 4! is
 // 4\*3\*2\*1.
@@ -17,7 +19,9 @@
 // beginning or the end; you would still have used vanilla. The formula for
 // combination is
 // 
-//              P(a,b) C(a,b) = ------.  b!
+//              P(a,b)
+//     C(a,b) = ------.
+//                b!
 // 
 // Design a program that asks users for two numbers, asks them whether they
 // want to calculate permutations or combinations, and prints out the result.
@@ -27,11 +31,12 @@
 // program should have error checking. Make sure that all erroneous inputs will
 // generate good error messages
 //
-// Comments: See 6.exercise.10.cpp
+// Comments: See 6.exercise.10.md
 
 #include "std_lib_facilities.h"
 
 const string ex_msg_pmul_with_negs = "check_pmul() called with negative integers.";
+const string ex_msg_product_overflows = "integer multiplication overflows.";
 
 int get_natural()
 // Currently it only reads a integer without any check. It's an straight
@@ -43,8 +48,11 @@ int get_natural()
 }
 
 bool check_pmul(int a, int b)
+// Partial implementation from SECURE CODING INT32C.
+// It do not throw since is its work to tell if multiplication could
+// be performed or not. Using exceptions is, I think, BLAHBLAHBLAH
 // Precondition:
-//      Both arguments must be positive
+//      Both arguments must be positive.
 {
     if (a < 0 || b < 0) 
         throw runtime_error(ex_msg_pmul_with_negs);
@@ -55,24 +63,28 @@ bool check_pmul(int a, int b)
     return true;
 }
 
-int prod(int a, int b)
-try
+int perm(int n, int k)
+// Calculates the k-permutations of n elements, what we call P(n, k).
+// This is not done with the factorial formula expressed in the statement but
+// with a limited product. See 6.exercise.10.md for details.
+// If either n or k equals zero, the permutation is 1.
+//
+// Preconditions:
+//      - n and k must be positive and n >= k. We will not check this
+//      precondition here since we assume it is assured by previous calls to
+//      get_natural() and check_input().
 {
-    int p = a;
-    for (int i = a+1; i <= b; ++i) {
-        if (check_pmul(p, i))
-            p *= i; 
-        else {
-            cout << "OVERFLOW!\n";
-            return -1;
-        }
-    }
-    return p;
-}
-catch(exception& e)
-{
-    cerr << e.what() << '\n';
-    throw;
+    if (n == 0 || k == 0) return 1;
+
+    int prod = 1;
+
+    for (int i = n-k+1; i <= n; ++i)
+        if (check_pmul(prod, i))
+            prod *= i;
+        else
+            throw runtime_error(ex_msg_product_overflows);
+
+    return prod;
 }
 
 int main(void)
@@ -87,7 +99,7 @@ try
         setc = get_natural();
         cout << "Subset cardinality?\n";
         subc = get_natural();
-        cout << prod(setc, subc) << '\n'; 
+        cout << perm(setc, subc) << '\n'; 
     }
     
     return 0;
