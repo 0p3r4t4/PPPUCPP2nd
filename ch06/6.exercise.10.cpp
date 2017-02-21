0981cc3e070816a852037ad13128bceb93ff0e42
0p3r4t4 <gonrodda@gmail.com>
2017-02-21 16:25:06 +0100

    Changes to exercise 10 from chapter 6 after static analysis.

--- ch06/6.exercise.10.cpp
+++ ch06/6.exercise.10.cpp
@@ -46,10 +46,7 @@ const string msg_subc_get = "Subset cardinality (n)? ";
 const string msg_calc_option = "What type of calculation do you want? (p)ermutation, (c)ombination or (b)oth? ";
 const string msg_again_option = "Do you want to perform a new calculation (y/n)? ";
 
-static const vector<char> calc_options = {'p', 'P', 'c', 'C', 'b', 'B'};
-static const vector<char> again_options = {'y', 'Y', 'n', 'N'};
-
-bool is_in(const char c, const vector<char> v)
+bool is_in(const char c, const vector<char>& v)
 // Returns true if character c is contained in vector<char> v and returns false
 // otherwise.
 {
@@ -89,7 +86,7 @@ int get_natural(const string& msg)
     }
 }
 
-char get_option(const string& msg, const vector<char> v)
+char get_option(const string& msg, const vector<char>& v)
 // Asks the user (printing the message msg) for a char value (option).
 // It keeps asking until the answer is a char value that is present in vector
 // v or EOF is reached or signales, in which case it throws and error.
@@ -101,10 +98,10 @@ char get_option(const string& msg, const vector<char> v)
         cin >> c;
         if (cin) {
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            if (is_in(c, v))
-                return c;
-            else
+            if (!is_in(c, v))
                 cout << c << " is not a valid option.\n";
+            else
+                return c;
         }
         else {
             // If cin fails reading a char, I guess the only way is to
@@ -125,10 +122,7 @@ bool check_pmul(int a, int b)
     if (a < 0 || b < 0) 
         error("check_pmul(): " + err_msg_pmul_with_negs);
 
-    if ( a > (numeric_limits<int>::max() / b) )
-        return false;
-
-    return true;
+    return !( a > (numeric_limits<int>::max() / b) );
 }
 
 int perm(int n, int k)
@@ -189,12 +183,11 @@ int comb(int n, int k)
     return prod;
 }
 
-int main(void)
+int main()
 try
 {
-    int setc = 0;
-    int subc = 0;
-    char option = ' ';
+    const vector<char> calc_options = {'p', 'P', 'c', 'C', 'b', 'B'};
+    const vector<char> again_options = {'y', 'Y', 'n', 'N'};
 
     cout << "Calculator for k-permutation and k-combination of n elements\n"
             "without repetition. Beware of large numbers, the calculator\n"
@@ -202,10 +195,10 @@ try
 
     while (cin) {
         cout << endl;
-        setc = get_natural(msg_setc_get);
-        subc = get_natural(msg_subc_get);
+        int setc = get_natural(msg_setc_get);
+        int subc = get_natural(msg_subc_get);
         
-        option = get_option(msg_calc_option, calc_options);
+        char option = get_option(msg_calc_option, calc_options);
         switch(option) {
             case 'p':
             case 'P':
