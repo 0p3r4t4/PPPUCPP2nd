57959b30f326959cac5faedbe83c8b664f2fa62e
0p3r4t4 <gonrodda@gmail.com>
2017-04-04 00:03:07 +0200

    Cleanup of comment from chapter 7 drill, step 1, to not obfuscate next steps. Add original file to compare.

---
+++ ch07/7.drill.2.calculator08buggy.cpp
@@ -0,0 +1,283 @@
+// 7.drill.2.calculator08buggy.cpp
+//
+//  2. Go through the entire program and add appropriate comments.
+//  3. As you commented, you found errors (deviously inserted especially for you
+//     to find). Fix them; they are not in the text of the book.
+//  4. Testing: prepare a set of inputs and use them to test the calculator. Is
+//     your list pretty complete? What should you look for? Include negative
+//     values, 0, very small, very large, and “silly” inputs.
+//  5. Do the testing and fix any bugs that you missed when you commented.
+//  6. Add a predefined name k meaning 1000.
+//  7. Give the user a square root function sqrt(), for example, sqrt(2+6.7).
+//     Naturally, the value of sqrt(x) is the square root of x; for example,
+//     sqrt(9) is 3. Use the standard library sqrt() function that is available
+//     through the header std_lib_facilities.h. Remember to update the comments,
+//     including the grammar.
+//  8. Catch attempts to take the square root of a negative number and print an
+//     appropriate error message.
+//  9. Allow the user to use pow(x,i) to mean “Multiply x with itself i times”;
+//     for example, pow(2.5,3) is 2.5*2.5*2.5.  Require i to be an integer using
+//     the technique we used for %.
+// 10. Change the “declaration keyword” from let to #.
+// 11. Change the “quit keyword” from quit to exit. That will involve defining
+//     a string for quit just as we did for let in §7.8.2.
+
+/*
+	calculator08buggy.cpp
+
+	Helpful comments removed.
+
+	We have inserted 3 bugs that the compiler will catch and 3 that it won't.
+*/
+
+#include "std_lib_facilities.h"
+
+class Token {
+public:
+	char kind;
+	double value;
+	string name;
+
+	Token(char ch) :kind{ch} { }
+	Token(char ch, double val) :kind{ch}, value{val} { }
+	Token(char ch, string n) : kind{ch}, name{n} { }
+};
+
+class Token_stream {
+public:
+    Token_stream() :full{false}, buffer{0} { }
+	Token get();
+	void putback(Token t) { buffer=t; full=true; }
+	void ignore(char c);
+private:
+	bool full;
+	Token buffer;
+};
+
+const char let = 'L';
+const char quit = 'Q';
+const char print = ';';
+const char number = '8';
+const char name = 'a';
+const string declkey = "let";
+const string quitkey = "quit";
+
+Token Token_stream::get()
+{
+	if (full) { full=false; return buffer; }
+	char ch;
+	cin >> ch;
+	switch (ch) {
+	case '(':
+	case ')':
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '%':
+	case ';':
+	case '=':
+		return Token{ch};
+	case '.':
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	{	cin.putback(ch);
+		double val;
+		cin >> val;
+		return Token{number, val};
+	}
+	default:
+		if (isalpha(ch)) {
+			string s;
+			s += ch;
+			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
+			cin.putback(ch);
+			if (s == declkey) return Token{let};	
+			if (s == quitkey) return Token{quit};
+			return Token{name,s};
+		}
+		error("Bad token");
+	}
+}
+
+void Token_stream::ignore(char c)
+{
+	if (full && c == buffer.kind) {
+		full = false;
+		return;
+	}
+	full = false;
+
+	char ch;
+	while (cin>>ch)
+		if (ch == c) return;
+}
+
+class Variable {
+public:
+	string name;
+	double value;
+};
+
+vector<Variable> var_table;	
+
+double get_value(string s)
+{
+	for (const Variable& v : var_table)
+	    if (v.name == s) return v.value;
+	error("get: undefined name ", s);
+}
+
+void set_value(string s, double d)
+{
+    for (Variable& v : var_table)
+        if (v.name == s) {
+            v.value = d;
+            return;
+        }
+	error("set: undefined name ", s);
+}
+
+bool is_declared(string s)
+{
+	for (const Variable& v : var_table)
+	    if (v.name == s) return true;
+	return false;
+}
+
+Token_stream ts;
+
+double expression();
+
+double primary()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case '(':
+	{	double d = expression();
+		t = ts.get();
+		if (t.kind != ')') error("')' expected");
+		return d;
+	}
+	case '-':
+		return - primary();
+	case number:
+		return t.value;
+	case name:
+		return get_value(t.name);
+	default:
+		error("primary expected");
+	}
+}
+
+double term()
+{
+	double left = primary();
+	while(true) {
+		Token t = ts.get();
+		switch(t.kind) {
+		case '*':
+			left *= primary();
+			break;
+		case '/':
+		{	double d = primary();
+			if (d == 0) error("divide by zero");
+			left /= d;
+			break;
+		}
+		default:
+			ts.putback(t);
+			return left;
+		}
+	}
+}
+
+double expression()
+{
+	double left = term();
+	while(true) {
+		Token t = ts.get();
+		switch(t.kind) {
+		case '+':
+			left += term();
+			break;
+		case '-':
+			left -= term();
+			break;
+		default:
+			ts.putback(t);
+			return left;
+		}
+	}
+}
+
+double declaration()
+{
+	Token t = ts.get();
+	if (t.kind != 'a') error ("name expected in declaration");
+	string name = t.name;
+	if (is_declared(name)) error(name, " declared twice");
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("= missing in declaration of " ,name);
+	double d = expression();
+	var_table.push_back(Variable{name,d});
+	return d;
+}
+
+double statement()
+{
+	Token t = ts.get();
+	switch(t.kind) {
+	case let:
+		return declaration();
+	default:
+		ts.putback(t);
+		return expression();
+	}
+}
+
+void clean_up_mess()
+{
+	ts.ignore(print);
+}
+
+const string prompt = "> ";
+const string result = "= ";
+
+void calculate()
+{
+	while(true) try {
+		cout << prompt;
+		Token t = ts.get();
+		while (t.kind == print) t = ts.get();
+		if (t.kind == quit) return;
+		ts.putback(t);
+		cout << result << statement() << '\n';
+	}
+	catch(exception& e) {
+		cerr << e.what() << '\n';
+		clean_up_mess();
+	}
+}
+
+int main()
+try {
+    calculate();
+    return 0;
+}
+catch (exception& e) {
+    cerr << "exception: " << e.what() << '\n';
+    return 1;
+}
+catch (...) {
+    cerr << "exception\n";
+	return 2;
+}
