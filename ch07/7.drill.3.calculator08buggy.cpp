d42a15f14e72d66cd37086827d9ab63485c29abb
0p3r4t4 <gonrodda@gmail.com>
2017-04-05 03:04:24 +0200

    Step 3 from chapter 7 Drill.

---
+++ ch07/7.drill.3.calculator08buggy.cpp
@@ -0,0 +1,322 @@
+// 7.drill.3.calculator08buggy.cpp
+//
+//  3. As you commented, you found errors (deviously inserted especially for you
+//     to find). Fix them; they are not in the text of the book.
+
+// This program implements a basic expression calculator based in this input
+// grammar:
+//
+//  Calculation:
+//      Statement
+//      Print
+//      Quit
+//      Calculation Statement
+//  Statement:
+//      Declaration
+//      Expression
+//  Print:
+//      ";"
+//  Quit:
+//      "quit"
+//  Declaration:
+//      "let" Name "=" Expression
+//  Expression:
+//      Term
+//      Expression "+" Term
+//      Expression "-" Term
+//  Term:
+//      Primary
+//      Term "*" Primary
+//      Term "/" Primary
+//  Primary:
+//      Number
+//      "("Expression")"
+//      "-" Primary
+//      "+" Primary
+//      Name
+//  Number:
+//      [floating-point-literal]
+//  Name:
+//      [alphabetic-char]
+//      Name[alphabetic-char]
+//      Name[digit-char]
+//
+// Input comes from cin through the Token_stream called ts.
+
+#include "std_lib_facilities.h"
+
+// Models a grammar token from input
+class Token {
+public:
+    // kind is always needed while value and name are initialized depending on
+    // the needs of each kind. Thus, three distinct constructors are defined.
+	char kind;  
+	double value;
+	string name;
+
+	Token(char ch) :kind{ch} { }
+	Token(char ch, double val) :kind{ch}, value{val} { }
+	Token(char ch, string n) : kind{ch}, name{n} { }
+};
+
+// Models cin as a Token stream
+class Token_stream {
+public:
+    Token_stream() :full{false}, buffer{0} { }
+	Token get();
+	void putback(Token t) { buffer=t; full=true; }
+	void ignore(char c);
+private:
+	bool full;
+	Token buffer;
+};
+
+// Token kinds - Arbitrarily chosen
+const char let = 'L';
+const char quit = 'Q';
+const char print = ';';
+const char number = '8';
+const char name = 'a';
+// Keywords
+const string declkey = "let";
+const string quitkey = "quit";
+
+Token Token_stream::get()
+// Processes cin to get terminal symbols from the implemented grammar
+{
+    // If already have a token buffered, return it and make
+    // room for the next one
+	if (full) { full=false; return buffer; }
+
+	char ch;
+	cin >> ch;
+	switch (ch) {
+	case '(':
+	case ')':
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '%':
+	case ';':
+	case '=':
+		return Token{ch};   // These literals directly define Token kind
+	case '.':
+	case '0': case '1': case '2': case '3': case '4':
+	case '5': case '6': case '7': case '8': case '9':   // Numeric literal
+	{	cin.putback(ch);
+		double val;
+		cin >> val;
+		return Token{number, val};
+	} 
+	default:
+	    // We can also expect strings. These to be keywords for declaration and
+	    // exiting the program or variable names.
+		if (isalpha(ch)) {
+			string s;
+			s += ch;
+			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
+			cin.putback(ch);
+			if (s == declkey) return Token{let};	
+			if (s == quitkey) return Token{quit};
+			return Token{name,s};
+		}
+		error("Bad token");
+	}
+}
+
+void Token_stream::ignore(char c)
+// Clear input until c or '\n' is found
+{
+    // First inspect if a Token of c kind is buffered
+	if (full && c == buffer.kind) {
+		full = false;
+		return;
+	}
+	// Dismiss buffered token
+	full = false;
+
+    // and work directly on cin
+    // DRILL 3. I find more useful for ignore to consider new line as limit for
+    // ignore. This causes user to be prompted again in case of input a name
+    // not yet defined (in the original you must insert a print command for
+    // prompt to be recovered). As it is, >> operator doesn't do the work and I
+    // must use con.get() to detect the newline char.
+	char ch{' '};
+    while (ch != c && ch != '\n')
+        ch = cin.get();
+    return;
+}
+
+class Variable {
+public:
+	string name;
+	double value;
+};
+
+vector<Variable> var_table;	
+
+double get_value(string s)
+{
+	for (const Variable& v : var_table)
+	    if (v.name == s) return v.value;
+	error("get: undefined name ", s);
+}
+
+void set_value(string s, double d)
+{
+    for (Variable& v : var_table)
+        if (v.name == s) {
+            v.value = d;
+            return;
+        }
+	error("set: undefined name ", s);
+}
+
+bool is_declared(string s)
+{
+	for (const Variable& v : var_table)
+	    if (v.name == s) return true;
+	return false;
+}
+
+Token_stream ts;
+
+double expression();
+
+double primary()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case '(':
+	{	double d = expression();
+		t = ts.get();
+		if (t.kind != ')') error("')' expected");
+		return d;
+	}
+	case '-':   // Negative signed numbers
+		return - primary();
+	// DRILL 3. Not an error but it seems stupid to include negative signed
+	// numbers and don't do the same with positive signed numbers
+	case '+':   // Positive signed numbers
+		return + primary();
+	case number:
+		return t.value;
+    case name:  // Variable: get value from table
+		return get_value(t.name);
+	default:
+		error("primary expected");
+	}
+}
+
+double term()
+{
+	double left = primary();
+	while(true) {
+		Token t = ts.get();
+		switch(t.kind) {
+		case '*':
+			left *= primary();
+			break;
+		case '/':
+		{	double d = primary();
+			if (d == 0) error("divide by zero");
+			left /= d;
+			break;
+		}
+		default:
+			ts.putback(t);
+			return left;
+		}
+	}
+}
+
+double expression()
+{
+	double left = term();
+	while(true) {
+		Token t = ts.get();
+		switch(t.kind) {
+		case '+':
+			left += term();
+			break;
+		case '-':
+			left -= term();
+			break;
+		default:
+			ts.putback(t);
+			return left;
+		}
+	}
+}
+
+double declaration()
+{
+    // Check part by part of Declaration gramamr rule behind "let"
+	Token t = ts.get();
+	// DRILL 3. Get rid of magic constant 'a' with the already defined constant
+	// name. It implies to change previous inner function variable name to
+	// var_name and avoid conflicts.
+	if (t.kind != name) error ("name expected in declaration");
+	string var_name = t.name;
+	if (is_declared(var_name)) error(var_name, " declared twice");
+
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("= missing in declaration of " ,var_name);
+
+	double d = expression();
+	var_table.push_back(Variable{var_name,d});
+	return d;
+}
+
+double statement()
+{
+	Token t = ts.get();
+	switch(t.kind) {
+	case let:
+		return declaration();
+	default:
+		ts.putback(t);
+		return expression();
+	}
+}
+
+void clean_up_mess()
+{
+	ts.ignore(print);   // Discard input until a print command or a new line
+	                    // is found (inclusive).
+}
+
+const string prompt = "> ";
+const string result = "= ";
+
+void calculate()
+{
+	while(true) 
+	try {
+		cout << prompt;
+		Token t = ts.get();
+		while (t.kind == print) t = ts.get();   // Remove print commands
+		if (t.kind == quit) return;
+		ts.putback(t);
+		cout << result << statement() << '\n';
+	}
+	catch(exception& e) {
+		cerr << e.what() << '\n';
+		clean_up_mess();    // Discard remaining input and prompt user again
+	}
+}
+
+int main()
+try {
+    calculate();
+    return 0;
+}
+catch (exception& e) {
+    cerr << "exception: " << e.what() << '\n';
+    return 1;
+}
+catch (...) {
+    cerr << "Uknown exception!\n";
+	return 2;
+}
