58f8d6f906101d8aa943f005fc2d2fa687fb71b7
0p3r4t4 <gonrodda@gmail.com>
2017-05-08 19:24:56 +0200

    Coffee work on exercise 7 from chapter 8.

--- ch08/8.exercise.07.cpp
+++ ch08/8.exercise.07.cpp
@@ -10,8 +10,49 @@
 
 #include "std_lib_facilities.h"
 
+constexpr size_t name_no{5};
+
+void get_names(vector<string>& name)
+{
+    cout << "Write five people names ...\n";
+    for (int i = 0; i < name_no; ++i) {
+        cout << ": ";
+        string n;
+        cin >> n;
+        name.push_back(n);
+    }
+}
+
+void get_ages(const vector<string>& name, vector<double>& age)
+{
+    cout << "Give me the ages of each person, please ...\n";
+    for (string n : name) {
+        cout << "How old is " << n << "? ";
+        double a{0}; 
+        while (!(cin >> a)) {
+            cout << "Sorry, I don't understand you! How old is " << n << "? ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        age.push_back(a);
+    }
+}
+
 int main()
 try {
+    vector<string> name;
+    vector<double> age;
+
+    get_names(name);
+    get_ages(name, age);
+
+    for (auto e : name)
+        cout << e << ' ';
+    cout << '\n';
+    for (auto e : age)
+        cout << e << ' ';
+    cout << '\n';
+
     return 0;
 }
 catch(exception& e)
