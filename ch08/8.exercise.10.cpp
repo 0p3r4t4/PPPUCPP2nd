3185fef22afa39ded584c702d7f0e96558f02afc
0p3r4t4 <gonrodda@gmail.com>
2017-05-11 01:51:17 +0200

    Finished exercise 10 from chapter 8.

--- ch08/8.exercise.10.cpp
+++ ch08/8.exercise.10.cpp
@@ -4,15 +4,31 @@
 
 #include "std_lib_facilities.h"
 
+void print(const string& label, const vector<double>& data)
+// Only read arguments, so it safe to pass them by const-reference
+{
+    cout << label << ": { ";
+    for (double d : data)
+        cout << d << ' ';
+    cout << "}\n";
+}
+
 double maxv(const vector<double>& v)
 {
-    double max{0};
+    double max{v[0]};   // We could not make assumptions about vector values
+                        // and so we must initalize the max track to an element
+    for (double d : v)
+        if (d > max) max = d;
+
     return max;
 }
 
 int main()
 try {
-    vector<double> test_data{1, 2, 3};
+    vector<double> test_data{-5, 0, 6.7, -1000, 10.1e5, 10e5};
+
+    print("Test data", test_data);
+    cout << "Largest element from data: " << maxv(test_data) << '\n';
     
     return 0;
 }
