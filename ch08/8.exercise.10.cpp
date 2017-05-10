d29de00ec89ab34cd518ee4278eb1d34cd3cc40c
0p3r4t4 <gonrodda@gmail.com>
2017-05-10 02:26:27 +0200

    Prepare exercise 10 from chapter 8.

---
+++ ch08/8.exercise.10.cpp
@@ -0,0 +1,28 @@
+// 8.exercise.10.cpp
+//
+//  Write a function maxv() that returns the largest element of a vector.
+
+#include "std_lib_facilities.h"
+
+double maxv(const vector<double>& v)
+{
+    double max{0};
+    return max;
+}
+
+int main()
+try {
+    vector<double> test_data{1, 2, 3};
+    
+    return 0;
+}
+catch(exception& e)
+{
+    cerr << e.what() << '\n';
+    return 1;
+}
+catch(...)
+{
+    cerr << "Unknown exception!!\n";
+    return 2;
+}
