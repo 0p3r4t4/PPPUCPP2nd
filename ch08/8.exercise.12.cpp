f45af515a00b66eefa486d2426b4dee50787ad7f
0p3r4t4 <gonrodda@gmail.com>
2017-05-12 01:42:40 +0200

    Work in progress with exercise 12 from chapter 8.

---
+++ ch08/8.exercise.12.cpp
@@ -0,0 +1,52 @@
+// 8.exercise.12.cpp
+//
+//  Improve print_until_s() from §8.5.2. Test it. What makes a good set of test
+//  cases? Give reasons. Then, write a print_until_ss() that prints until it
+//  sees a second ocurrence of its quit argument.
+//
+// COMMENTS
+//
+//  Since we don't modify the parameter v we can pass it by const-reference and
+//  the same goes about the quit parameter.
+//
+//  Set to test ...
+//    Test with empty vectors and strings.
+//    Test with quit string being the first or the last element of the vector.
+//    Test with variables (or constants) as arguments.
+
+#include "std_lib_facilities.h"
+
+void print(const vector<string>& v, const string& quit)
+{
+    cout << "Printing vector { ";
+    for (string s : v) cout << s << ' ';
+    cout << "} UNTIL the first appareance of '" << quit << "' ...\n";
+}
+
+void print_until_s(const vector<string>& v, const string& quit)
+{
+    print(v, quit);
+    for (string s : v) {
+        if (s == quit) return;
+        cout << '\t' << s << '\n';
+    }
+}
+
+int main()
+try
+{
+    vector<string> test_data{"1", "2", "3", "4", "5"};
+    print_until_s({"Hi!", "Hello", "Bye", "quit", "Until tomorrow"}, "quit");
+    print_until_s({"Hi!", "Hello", "Bye", "quit", "Until tomorrow"}, "quit");
+    return 0;
+}
+catch(exception& e)
+{
+    cerr << e.what() << '\n';
+    return 1;
+}
+catch(...)
+{
+    cerr << "Unknown exception!!\n";
+    return 2;
+}
