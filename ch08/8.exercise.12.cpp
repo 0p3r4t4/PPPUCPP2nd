976ef8cc8d3cf7f02e28981adc1349d2aa07bbc3
Dan González Rodríguez <gonrodda@gmail.com>
2017-05-13 14:06:41 +0200

    Added print_until_ss() to exercise 12 from chapter 8.
    Sleep betrayed me and I completely missed it.

--- ch08/8.exercise.12.cpp
+++ ch08/8.exercise.12.cpp
@@ -17,18 +17,30 @@
 
 #include "std_lib_facilities.h"
 
-void print(const vector<string>& v, const string& quit)
+void print(const vector<string>& v, const string& quit, const string& which)
 {
     cout << "Printing vector { ";
     for (string s : v) cout << s << ' ';
-    cout << "} UNTIL the first appareance of '" << quit << "' ...\n";
+    cout << "} UNTIL the " << which << " appareance of '" << quit << "' ...\n";
 }
 
 void print_until_s(const vector<string>& v, const string& quit)
 {
-    print(v, quit);     // Added for test information
+    print(v, quit, "first");    // Added for test information
     for (string s : v) {
         if (s == quit) return;
+        cout << '\t' << s << '\n';
+    }
+}
+
+void print_until_ss(const vector<string>& v, const string& quit)
+{
+    print(v, quit, "second");   // Added for test information
+    bool seen{false};           // Tracks first occurrence of quit
+    for (string s : v) {
+        if (s == quit)
+            if (seen) return;
+            else seen = true;
         cout << '\t' << s << '\n';
     }
 }
@@ -40,11 +52,17 @@ try
     const vector<string> test_data_c{};
     string quit{"exit"};
     
+    cout << "\nprint_until_s() tests:\n\n";
     print_until_s({"Hi!", "Hello", "Bye", "quit", "Until tomorrow"}, "quit");
     print_until_s(test_data, "");
     print_until_s(test_data_c, quit);
     print_until_s({"quit", "Hello", "Bye", "quit", "Until tomorrow"}, "quit");
     print_until_s({"Hi!", "Hello", "Bye", "quit", quit}, quit);
+
+    cout << "\nprint_until_ss() tests:\n\n";
+    print_until_ss({"Hi!", "Hello", "Bye", "quit", "Until tomorrow", "quit",
+                    "No more"}, "quit");
+    print_until_ss({"Hi!", "Hello", quit, "Bye", "exit", "quit", quit}, quit);
 
     return 0;
 }
