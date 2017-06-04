81aa026ff9304926ac1e5cbdbca660ddb68a67a9
0p3r4t4 <gonrodda@gmail.com>
2017-06-04 23:08:29 +0200

    Finished exercise 14 from chapter 9.

--- ch09/9.exercise.14.cpp
+++ ch09/9.exercise.14.cpp
@@ -12,13 +12,14 @@
 //  What operations will be implemented? Addition and substraction for sure.
 //  As for multiplication and division, we must think about what type of
 //  operands make sense:
-//      (Money * Money)                     does not make sense to me
-//      (Money * int) and (int * Money)     are perfectly legit
-//      (Money / int)                       makes sense
-//      (Money / Money)                     could make sense to distribute
+//      (Money * Money)                         does not make sense to me
+//      (Money * double) and (double * Money)   are perfectly legit
+//      (Money / double)                        makes sense
+//      (Money / Money)                         could make sense to distribute
+//                                              or find ratios between amounts
 //
-//  As for the 4/5 rounding rule goes, only the division operator is affected
-//  (as far as only a currency is involved).
+//  As for the 4/5 rounding rule goes, only the multiplication and division
+//  operators are affected (as far as only a currency is involved).
 //
 //  Do we want to manage negative amounts of money? It is interesting to manage
 //  debts, so yes.
@@ -27,54 +28,68 @@
 
 constexpr char currency{'$'};
 
+long int iround(double n) 
+{
+    return (n < 0.0) ? (n - 0.5) : (n + 0.5);
+}
+
 class Money {
 public:
+    class Division_by_zero { }; // custom exception
+
+    Money(long int units, long int cents);  // constructor on units and cents
     Money(long int amount);     // constructor on number of cents
     Money();                    // default constructor, amount 0
 
     long int amount() const { return m_amount; };    // accessor
+    
+    long int units() const { return m_amount/100; }; // data retrieve
+    long int cents() const { return m_amount%100; };
 private:
     long int m_amount;     // amount on money on cents
 };
 
+Money::Money(long int units, long int cents) : m_amount{units*100+cents} { };
 Money::Money(long int amount) : m_amount{amount} { };
 Money::Money() : m_amount{0} { };
 
-Money operator-(const Money& m)
+Money operator-(const Money& rhs)
 {
-    return(Money{-m.amount()});
+    return Money{-rhs.amount()};
 }
 
 Money operator+(const Money& lhs, const Money& rhs)
 {
-    return(Money{lhs.amount()+rhs.amount()});
+    return Money{lhs.amount()+rhs.amount()};
 }
 
 Money operator-(const Money& lhs, const Money& rhs)
 {
-    return(Money{lhs.amount()-rhs.amount()});
+    return Money{lhs.amount()-rhs.amount()};
 }
 
-Money operator*(const Money& lhs, long int rhs)
+Money operator*(const Money& lhs, double rhs)
 {
-    return(Money{lhs.amount()*rhs});
+    return Money{ iround(lhs.amount()*rhs) };
 }
 
-Money operator*(long int lhs, const Money& rhs)
+Money operator*(double lhs, const Money& rhs)
 {
-    return(Money{lhs*rhs.amount()});
+    return Money{ iround(lhs*rhs.amount()) };
 }
 
-Money operator/(const Money& lhs, long int rhs)
+Money operator/(const Money& lhs, double rhs)
 {
-    return(Money{lhs.amount()/rhs});
-    // TODO: Rounding
+    if (rhs == 0) throw Money::Division_by_zero{};
+
+    return Money{ iround(lhs.amount()/rhs) };
 }
 
-long int operator/(const Money& lhs, const Money& rhs)
-// Times of money distribution
+double operator/(const Money& lhs, const Money& rhs)
 {
-    return(lhs.amount()/rhs.amount());
+    if (rhs.amount() == 0) throw Money::Division_by_zero{};
+
+    return (double(lhs.amount())/rhs.amount());
 }
 
 bool operator==(const Money& lhs, const Money& rhs)
@@ -89,38 +104,46 @@ bool operator!=(const Money& lhs, const Money& rhs)
 
 ostream& operator<<(ostream& os, const Money& money)
 {
-    os << currency << money.amount()/100 << '.';
-
-    long int cents{money.amount()%100};
-    if (cents < 10) os << '0'; // we could need a leading 0
-    os << cents;
+    os << currency << money.units() << '.';
+    if (abs(money.cents()) < 10) os << '0'; // we could need a leading 0
+    os << abs(money.cents());
 
     return os;
 }
 
 istream& operator>>(istream& is, Money& money)
 {
-    long int units, cents;
+    long int units{0};
     char cur, dot;
+    char d, u;
 
-    is >> cur >> units >> dot >> cents;
+    is >> cur >> units >> dot >> d >> u;
     if (!is) return is;
-    if (units != currency || dot != '.') {
+    if (cur != currency || dot != '.' || !isdigit(d) || !isdigit(u)) {
         is.clear(ios_base::failbit);
         return is;
     }
+    // if we read an integer after the dot we could find out that
+    // 00000015 results to be 15. A solution will be to put back the
+    // dot and read a double. This has its own set of new problems, so
+    // I will read two chars and compose the cents. Pure brute force.
+    int cents{(d-'0')*10+(u-'0')};
 
-    money = Money{};
+    if (units < 0) cents = -cents;  // only units have sign, but cents matter
+    money = Money{(units*100)+cents};
+
+    return is;
 }
 
 int main()
 try{
 
-    // Test Money*int, int*Money, Money/Money, Money/int, Money-Money and <<
-    Money budget{1000000*100};
-    Money wage{1500*100};
+    // Test Money*double, double*Money, Money/Money, Money/double, Money-Money
+    // and <<
+    Money budget{1000000,0};
+    Money wage{1500,0};
     long int months{18};
-    long int workers{budget/(wage*months)};
+    long int workers{ iround(budget/(wage*months)) };
     Money surplus{budget-(workers*wage*months)};
     cout << "The project's budget ascends to " << budget
          << " and it will be developed during " << months << " months.\n"
@@ -128,7 +151,57 @@ try{
          << "So we can hire " << workers << " developers.\n"
          << "The spare budget will be " << surplus << ".\n";
 
+    cout << '\n';
+
+    // Test Money*double, Money+Money and <<
+    Money product{134,0};
+    double vat = 0.21;
+    Money total{product+(product*vat)};
+    cout << "You have bought and paid by transference " << product
+         << " for a product. But you forgot the VAT (" << vat*100
+         << "%).\n" << "With VAT applied the product costs " << total
+         << " so you debt " << product-total << '\n';
+
+    cout << '\n';
+
+    // Test and justify Money/Money
+    Money after_tax{41,82};
+    Money before_tax{34,56};
+    double tax{(after_tax/before_tax)-1};
+    cout << "A product with a final price of " << after_tax << " and a price"
+         << " before taxes of " << before_tax << "\nhas been applied a "
+         << tax*100 << "% of taxes (" << after_tax-before_tax << ")\n";
+
+    cout << '\n';
+
+    // Test >> and << operators;
+    cout << "Write some quantities (examples: $123.04 or $-10.10, two digits"
+         << " for cents are mandatory. Quit with Ctrl+D).\n> ";
+    Money input{0};
+    while (cin) {
+        cin >> input;
+        if (cin.eof()) break;
+        if (cin.fail()) {
+            cout << "Bad input format\n> ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        else {
+            cout << '\t' << input << "\n> ";
+        }
+    }
+
+    cout << "\nBye!\n";
+
+    // Forced errors (division by zero)
+    //cout << Money{}/0;
+    //cout << Money{}/Money{};
     return 0;
+}
+catch(Money::Division_by_zero& e)
+{
+    cerr << "Money class: division by zero\n";
+    return 1;
 }
 catch(exception& e)
 {
