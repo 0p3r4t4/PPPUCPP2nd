5ecdb499bf224453d7f457fb4dddcfdab830d49b
0p3r4t4 <gonrodda@gmail.com>
2017-06-06 16:02:31 +0200

    Finished exercise 15 from chapter 9.

--- ch09/9.exercise.15.cpp
+++ ch09/9.exercise.15.cpp
@@ -9,17 +9,17 @@
 //
 // COMMENTS
 //
-//  So, how to manage data referred to currencies and exchange ratios? What I
-//  want to prioritize is ease of maintenance, so the needed data extends the
+//  So, how to manage data referred to currencies and exchange rates? What I
+//  want to prioritize is ease of maintenance so the needed data extends the
 //  fewer places the best. And I really don't know how to conciliate this with
 //  good practices and with we have learned from the book so far.
 //
 //  My approach is to encapsulate the data inside a class, Currency_data, that
 //  admits no further changes; every function member is const and private data
 //  member is const and static. We have read about static, but no so in depth.
-//  I just go through compiler error and warnigns to get there.
+//  I just go through compiler errors and warnigns to get there.
 //
-//  The way to use this class is to create a global object. Not very elegant,
+//  The way to use this class is to create a global object. Not very elegant
 //  but has been done earlier on the calculator. We need that object primarily
 //  for operator overloads on Money and there is no pass-by way to carry that
 //  object into the operator functions.
@@ -27,72 +27,87 @@
 //  You can argue me as much as you want and you'll have part of reason.
 //  Another way to do this, given the constness and staticness of
 //  Currency_data, is to create a member of that type in Money. The data will
-//  not be duplicated and it's efficient. But it feels more convoluted to me;
-//  Money class has no need for this data, only the helper functions do, so I
-//  don't see why Money would have to host a Currency_data member.
-//  Other way is to hardcode the currency data on as many functions as we need.
-//  But this goes against ease of maintenance, and we will face lengthy
+//  not be duplicated and it's storage efficient. But it feels more convoluted
+//  to me; Money class has no need for this data, only the helper functions do,
+//  so I don't see why Money would have to host a Currency_data member.  Other
+//  way is to hardcode the currency data on as many functions as we need.  But
+//  this goes against ease of maintenance, and we will face lengthy
 //  switch-case-statements before we add a bunch of currencies to the program.
-//
 //  
 //  I will take currency rates to USD and calculate the rest if needed. Of
-//  course currency conversion it's not so easy, and it will not be precise.
-//  Rates are not the same if you are buying or selling. Rates also differ
-//  (there no exact arithmetic relationship) if you are trading EUR to USD or
-//  USD to EUR. So I'll simplify based on rates from currencies to USD. We
-//  could expand Currency_data to treat these differences if we want to.
+//  course currency conversion is not so easy, and it won't be precise.  Rates
+//  are not the same if you are buying or selling. Rates also differ (there is
+//  no exact arithmetic relationship) if you are trading EUR to USD or USD to
+//  EUR. So I'll simplify based on rates from currencies to USD. We could
+//  expand Currency_data to treat these differences if we want to.
 //
 //  When I add the constructor Money(double, Currency) without removing the
 //  constructor Money(long int, Currency) and try to create an object with
-//  something like Money{0, Currency::USD}, compilers complains about an
+//  something like Money{0, Currency::USD}, compiler complains about an
 //  ambiguous call. The thing is that 0 is an int, and between the two
-//  candidates, it must be converted, so no one is better than the other.
-//  The solution is to suffix the integer literal to mark int as a long int, so
-//  Money{0L, Currency::USD} will disambiguate. C++ is turning difficult by
-//  moments.
+//  candidates, it must be converted or promoted, so no one is better than the
+//  other.  The solution is to suffix the integer literal to mark int as a long
+//  int, so Money{0L, Currency::USD} will disambiguate. C++ is turning
+//  difficult by moments.
 //  We could argue if we need the former constructor with long int now that we
 //  have a constructor with double that will simplify the >> operator overload
 //  on Money.
 //
-//  Now, with currency exchange rate being doubles and applied on all
+//  Exercise tell us to implement a floating-point initializer "as long as it
+//  can be exactly represented as a long int". Luckily I tested 4.56 on input,
+//  and discover that it isn't representable by double type (is represented as
+//  4.5599999999999994). I will not handle this because of the exercise
+//  statement, but to solve it some iround() or round() must be employed and
+//  operator>>(Money) reviewed.
+//
+//  Now, with currency exchange rates being doubles and applied on all
 //  arithmetic operators for Money, rounding is needed almost everywhere.
 
 #include "std_lib_facilities.h"
 
 namespace Financial {
 
-constexpr char currency{'$'};   // To be deleted
+// wide scope helper funcion
+long int iround(double n) 
+{
+    return (n < 0.0) ? (n - 0.5) : (n + 0.5);
+}
 
+// Currency enum class
 enum class Currency {
    USD, ARS, DKK, EUR, GBP
 };
 
+// Currency_data class
 class Currency_data { 
 public:
-    class Invalid_currency { };     // Exception
+    class Invalid_currency { };  // Exception
     struct row {
         Currency id;
-        string name;
-        double ratio;   // to dollars
+        string name;             // i.e. "GBP"
+        string long_name;        // i.e. "British Pounds"
+        double ratio;            // to US Dollars
     };
 
-    Currency currency(const string& name) const;     // get currency by name
-    const string& name(Currency currency) const;     // get name by id
-    double ratio(Currency currency) const;           // get ratio to US dollars
-    double ratio(Currency a, Currency b) const;      // get conversion ratio
+    Currency currency(const string& name) const;      // get currency by name
+    const string& name(Currency currency) const;      // get name by id
+    const string& long_name(Currency currency) const; // get long name by id
+    double ratio(Currency currency) const;            // get ratio to US dollars
+    double ratio(Currency a, Currency b) const;       // get conversion ratio
+    
+    void info() const;  // print information about currencies suportted
 
-    void print() const;      // Test function
 private:
     static const vector<row> data;
 };
 
-// Definition of Currency_data::data outside the class
+// Definition of static const Currency_data::data must be outside the class
 const vector<Currency_data::row> Currency_data::data {
-    row{Currency::USD, "USD", 1.0},       // US Dollar
-    row{Currency::ARS, "ARS", 0.06236},   // Argentine Peso
-    row{Currency::DKK, "DKK", 0.15164},   // Danish Krone
-    row{Currency::EUR, "EUR", 1.12798},   // Euro
-    row{Currency::GBP, "GBP", 1.28841}    // British Pound
+    row{Currency::USD, "USD", "US Dollar",      1.0},
+    row{Currency::ARS, "ARS", "Argentine Peso", 0.06236},
+    row{Currency::DKK, "DKK", "Danish Krone",   0.15164},
+    row{Currency::EUR, "EUR", "Euro",           1.12798},
+    row{Currency::GBP, "GBP", "British Pound",  1.28841}
 };
 
 // Global currency data provider
@@ -105,8 +120,7 @@ ostream& operator<<(ostream& os, const Currency currency)
     return os;
 }
 
-istream& operator>>(istream&is, Currency& currency)
-try
+istream& operator>>(istream& is, Currency& currency)
 {
     char c;
     string name;
@@ -114,24 +128,21 @@ try
     // beware the magic number!
     for (int i = 0; i < 3; ++i) {
         is >> c;
+        if (!is) return is;  // in case of EOF
         name += c;
     }
-    currency = currency_data.currency(name);    // may throw
-    return is;
-}
-catch(Currency_data::Invalid_currency& e)
-{
-    is.clear(ios_base::failbit);
+
+    try {
+        currency = currency_data.currency(name);  // may throw
+    }
+    catch(Currency_data::Invalid_currency& e) {
+        is.clear(ios_base::failbit);
+    }
+
     return is;
 }
 
 // Currency_data function members
-void Currency_data::print() const
-{
-    for (auto x : data)
-        cout << int(x.id) << ' ' << x.name << ' ' << x.ratio << '\n';
-}
-
 Currency Currency_data::currency(const string& name) const
 {
     for (const row& r : data)
@@ -144,6 +155,14 @@ const string& Currency_data::name(Currency currency) const
 {
     for (const row& r : data)
         if (r.id == currency) return r.name;
+
+    throw Invalid_currency{};
+}
+
+const string& Currency_data::long_name(Currency currency) const
+{
+    for (const row& r : data)
+        if (r.id == currency) return r.long_name;
 
     throw Invalid_currency{};
 }
@@ -167,16 +186,17 @@ double Currency_data::ratio(Currency a, Currency b) const
                                 // works fine for all cases.
 }
 
-// Global helper funcion
-long int iround(double n) 
+void Currency_data::info() const
 {
-    return (n < 0.0) ? (n - 0.5) : (n + 0.5);
+    for (const row& r: data)
+        cout << r.name << " (" << r.long_name << ")\n";
 }
 
+// Money class
 class Money {
 public:
     class Division_by_zero { }; // custom exception
-    class Invalid_amount { };   // Basically float point constructor does not
+    class Invalid_amount { };   // basically float point constructor does not
                                 // preserve amount argument
 
     // constructor on units and cents
@@ -206,7 +226,7 @@ Money::Money(long int amount, Currency currency)
 : m_amount{amount}, m_currency{currency} { };
 
 Money::Money(double amount, Currency currency)
-: m_amount{int(amount*100)}, m_currency{currency}
+: m_amount{(long int)(amount*100)}, m_currency{currency}
 {
     if (double(m_amount) != (amount*100)) throw Invalid_amount{};
 }
@@ -222,13 +242,13 @@ Money operator-(const Money& rhs)
 Money operator+(const Money& lhs, const Money& rhs)
 {
     // avoid unreadable one-liner
-    double ratio{currency_data.ratio(lhs.currency(), rhs.currency())};
+    double ratio{currency_data.ratio(rhs.currency(), lhs.currency())};
     return Money{iround(lhs.amount()+(rhs.amount()*ratio)), lhs.currency()};
 }
 
 Money operator-(const Money& lhs, const Money& rhs)
 {
-    double ratio{currency_data.ratio(lhs.currency(), rhs.currency())};
+    double ratio{currency_data.ratio(rhs.currency(), lhs.currency())};
     return Money{iround(lhs.amount()-(rhs.amount()*ratio)), lhs.currency()};
 }
 
@@ -253,33 +273,32 @@ double operator/(const Money& lhs, const Money& rhs)
 {
     if (rhs.amount() == 0) throw Money::Division_by_zero{};
 
-    double ratio{currency_data.ratio(lhs.currency(), rhs.currency())};
+    double ratio{currency_data.ratio(rhs.currency(), lhs.currency())};
     return (double(lhs.amount())/(rhs.amount()*ratio));
 }
 
 bool operator==(const Money& lhs, const Money& rhs)
 {
-    double ratio{currency_data.ratio(lhs.currency(), rhs.currency())};
+    double ratio{currency_data.ratio(rhs.currency(), lhs.currency())};
     return (lhs.amount() == iround(rhs.amount()*ratio));
 }
 
 bool operator!=(const Money& lhs, const Money& rhs)
 {
-    double ratio{currency_data.ratio(lhs.currency(), rhs.currency())};
+    double ratio{currency_data.ratio(rhs.currency(), lhs.currency())};
     return (lhs.amount() != iround(rhs.amount()*ratio));
 }
 
 ostream& operator<<(ostream& os, const Money& money)
 {
     os << money.currency() << money.units() << '.';
-    if (abs(money.cents()) < 10) os << '0'; // we could need a leading 0
+    if (abs(money.cents()) < 10) os << '0';  // we could need a leading 0
     os << abs(money.cents());
 
     return os;
 }
 
 istream& operator>>(istream& is, Money& money)
-try
 {
     double amount{0};
     Currency currency{Currency::USD};
@@ -289,13 +308,13 @@ try
     is >> amount;
     if (!is) return is;
 
-    money = Money{amount, currency};            // may throw
+    try {
+        money = Money{amount, currency};  // may throw
+    }
+    catch(Money::Invalid_amount& e) {
+        is.clear(ios_base::failbit);
+    }
 
-    return is;
-}
-catch(Money::Invalid_amount& e)  
-{
-    is.clear(ios_base::failbit);
     return is;
 }
 
@@ -305,8 +324,6 @@ using namespace Financial;
 
 int main()
 try{
-    currency_data.print(); // Silly test. REMOVE IT. Or not.
-
     // Currency_data::ratio() test
     cout << "USD -> USD - " << currency_data.ratio(Currency::USD, Currency::USD) << '\n';
     cout << "USD -> EUR - " << currency_data.ratio(Currency::USD, Currency::EUR) << '\n';
@@ -317,13 +334,37 @@ try{
     cout << "GBP -> EUR - " << currency_data.ratio(Currency::GBP, Currency::EUR) << '\n';
     cout << "EUR -> ARS - " << currency_data.ratio(Currency::EUR, Currency::ARS) << '\n';
 
+    cout << '\n';
+
+    // Test operators for distinct currencies
+    Money usd{1000.00, Currency::USD};
+    Money eur{1000.00, Currency::EUR};
+    Money gbp{1000.00, Currency::GBP};
+    Money ars{1000.00, Currency::ARS};
+    Money dkk{1000.00, Currency::DKK};
+    Money dkk_usd{
+        iround(usd.amount()*currency_data.ratio(Currency::USD, Currency::DKK)),
+        Currency::DKK
+    };
+    cout <<"-" << eur << " = " << -eur << '\n';
+    cout << eur << " + " << gbp << " = " << eur+gbp << '\n';
+    cout << usd << " - " << gbp << " = " << usd-gbp << '\n';
+    cout << dkk << " * " << 25 << " = " << dkk*25 << '\n';
+    cout << 25 << " * " << ars << " = " << 25*ars << '\n';
+    cout << ars << " / " << 10 << " = " << ars/10 << '\n';
+    cout << usd << " / " << dkk << " = " << usd/dkk << '\n';
+    if (usd == dkk_usd) cout << usd << " == " << dkk_usd << '\n';
+    if (usd != gbp) cout << usd << " != " << gbp << '\n'; 
+
+    cout << '\n';
+
     // Test Money*double, double*Money, Money/Money, Money/double, Money-Money
     // and <<
     Money budget{1000000,0,Currency::USD};
-    Money wage{1500,0,Currency::USD};
+    Money wage{9908,35,Currency::DKK};
     long int months{18};
     long int workers{ iround(budget/(wage*months)) };
-    Money surplus{budget-(workers*wage*months)};
+    Money surplus{budget-(workers*wage*months)};    // No currency: copy constructor?
     cout << "The project's budget ascends to " << budget
          << " and it will be developed during " << months << " months.\n"
          << "Each developer get " << wage << " per month. "
@@ -354,8 +395,10 @@ try{
     cout << '\n';
 
     // Test >> and << operators;
-    cout << "Write some quantities (examples: $123.04 or $-10.10, two digits"
-         << " for cents are mandatory. Quit with Ctrl+D).\n> ";
+    cout << "Write some quantities. Examples: EUR23.04 or GBP-10.\n"
+         << "Quit with Ctrl+D. Supported currenies are:\n\n";
+    currency_data.info();
+    cout << "\n> ";
     Money input{12.01,Currency::USD};
     while (cin) {
         cin >> input;
@@ -366,7 +409,10 @@ try{
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
         else {
-            cout << '\t' << input << "\n> ";
+            double ratio = currency_data.ratio(input.currency(), Currency::EUR);
+            cout << '\t' << input << "  ("
+                 << Money{iround(input.amount()*ratio), Currency::EUR}
+                 << ")\n> ";
         }
     }
 
