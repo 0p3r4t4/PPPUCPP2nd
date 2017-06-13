207aa9e328924f896014dcd7ed08639331d0feb4
0p3r4t4 <gonrodda@gmail.com>
2017-06-13 02:32:12 +0200

    Prepare exercise 3 from chapter 10.

---
+++ ch10/10.exercise.03.cpp
@@ -0,0 +1,29 @@
+// 10.exercise.03.cpp
+//
+//  Write a program that reads the data from raw_temps.txt created in exercise
+//  2 into a vector and then calculates the mean and median temperatures in
+//  your data set. Call this program temp_stats.cpp.
+//
+// COMMENTS
+//
+//  For the sake of standardization I will hold my naming schema, so the
+//  program will be 10.exercise.03.cpp and the file it reads will be
+//  10.exercise.02.output.
+
+#include "std_lib_facilities.h"
+
+int main()
+try
+{
+    return 0;
+}
+catch (exception& e)
+{
+    cerr << e.what() << '\n';
+    return 1;
+}
+catch (...)
+{
+    cerr << "Unknown exception!!\n";
+    return 2;
+}
