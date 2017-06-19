464818a3219a96df6b18d217c35e69c6b01486a7
0p3r4t4 <gonrodda@gmail.com>
2017-06-19 02:14:51 +0200

    Prepare exercise 6 from chapter 10.

---
+++ ch10/10.exercise.06.cpp
@@ -0,0 +1,21 @@
+// 10.exercise.06.cpp
+//
+//  Define a Roman_int class for holding Roman numerals (as ints) with a << and
+//  >>. Provide Roman_int with an as_int() member that returns the int value,
+//  so that if r is a Roman_int, we can write cout << "Roman" << r << "equals"
+//  << r.as_int() << '\n';.
+
+int main()
+try 
+{
+    return 0;
+}
+catch (exception& e)
+{
+    cerr << e.what() << '\n';
+    return 1;
+}
+catch (...)
+    cerr << "Unknown exception!!\n";
+    return 2;
+}
