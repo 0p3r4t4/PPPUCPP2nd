130e11e02b687a51c644efaa1530b2c956d6cb88
Dan González Rodríguez <gonrodda@gmail.com>
2017-07-09 14:32:56 +0200

    Finished exercise 9 from chapter 10.
    Also an small change in exercise 8 for an argument to be passed by const
    reference.

--- ch10/10.exercise.09.cpp
+++ ch10/10.exercise.09.cpp
@@ -1,44 +1,46 @@
-// 10.exercise.08.cpp
+// 10.exercise.09.cpp
 //
-//  Write a program that accepts two file names and produces a new file that is
-//  the contents of the first file followed by the contents of the second; that
-//  is, the program concatenates the two files.
-//
+//  Write a program that takes two files containing sorted whitespace-separated
+//  words and merges them, preserving order.
 
 #include "std_lib_facilities.h"
 
-void add_content_from(const string& name, ofstream& ost)
+void get_words(const string& name, vector<string>& words)
 {
     ifstream ist{name};
     if (!ist) error("Unable to open input file ", name);
     ist.exceptions(ist.exceptions()|ios_base::badbit);
 
-    while (!ist.eof()) {
-        char c = ist.get();     // char by char to preserve format. Unefficient.
-        if (!ist.eof()) ost << c;   // avoid a trash character when EOF is reached
-    }
+    for (string s; ist >> s;)
+        words.push_back(s);
 
     if (ist.eof()) ist.clear();     // File completely read
     if (ist.fail()) error("Not well formatted file ", name);    // Unlikely
 }
 
-void concat_files(const string& if_name_1, const string& if_name_2,
+void merge_files(const string& if_name_1, const string& if_name_2,
                   const string& of_name)
 {
     ofstream ost{of_name};
     if (!ost) error("Unable to open output file ", of_name);
 
-    add_content_from(if_name_1, ost);
-    add_content_from(if_name_2, ost);
+    vector<string> words;
+    get_words(if_name_1, words);
+    get_words(if_name_2, words);
+    sort(words);
+
+    for (size_t i = 0; i < words.size()-1; ++i)
+        ost << words[i] << ' ';
+    ost << words.back();
 }
 
-int main() 
+int main()
 try {
     string if_name_1{""};
     string if_name_2{""};
     string of_name{""};
 
-    cout << "Concatenate two files.\n";
+    cout << "Merge whitespace-separated sorted words from two files.\n";
     cout << "First input file name: ";
     cin >> if_name_1;
     cout << "Second input file name: ";
@@ -46,10 +48,9 @@ try {
     cout << "Output file name: ";
     cin >> of_name;
 
-    cout << "Concatenating ...";
-    concat_files(if_name_1, if_name_2, of_name);
+    cout << "Merging ...";
+    merge_files(if_name_1, if_name_2, of_name);
     cout << "\nDone.\n";
-
     return 0;
 }
 catch (exception& e)
